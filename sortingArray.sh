f1c8e4264c6d6cbef80d745980a5800058ea8f04
Darpan Madlani <Darpan@proclink.com>
2021-12-02 10:11:51 +0530

    sorting array's computations

--- sortingArray.sh
+++ sortingArray.sh
@@ -13,4 +13,19 @@ ans[((cnt++))]=$(($a*$b+$c));
 ans[((cnt++))]=$(($c+$a/$b));
 ans[((cnt++))]=$(($a%$b+$c));
 
-echo ${ans[@]};
+echo "Original array : "${ans[@]};
+
+for ((i=0;i<${#ans[@]};i++))
+do
+	for ((j=i+1;j<${#ans[@]};j++))
+	do
+		if [ ${ans[i]} -gt ${ans[j]} ]
+		then
+			temp=${ans[i]};
+			ans[i]=${ans[j]};
+			ans[j]=$temp;
+		fi
+	done
+done
+
+echo "Sorted array : "${ans[@]};
