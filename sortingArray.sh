8e00eae3acdd1e0a25d04b47afe9ec5e78b871e9
Darpan Madlani <Darpan@proclink.com>
2021-12-02 09:43:14 +0530

    a+b*c computation

--- sortingArray.sh
+++ sortingArray.sh
@@ -1,3 +1,11 @@
-read -p "Enter value a : " a
-read -p "Enter value b : " b
-read -p "Enter value c : " c
+#read -p "Enter value a : " a
+#read -p "Enter value b : " b
+#read -p "Enter value c : " c
+
+a=$((RANDOM%100));
+b=$((RANDOM%100));
+c=$((RANDOM%100));
+
+ans=$(($a+$b*$c));
+
+echo $ans;
