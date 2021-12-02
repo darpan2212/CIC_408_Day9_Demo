2f4a2d84d02de8c2fbf2e6a10bf7bb6a7403695d
Darpan Madlani <Darpan@proclink.com>
2021-12-02 09:54:23 +0530

    compute all four calculations and storing it in an array

--- sortingArray.sh
+++ sortingArray.sh
@@ -2,10 +2,15 @@
 #read -p "Enter value b : " b
 #read -p "Enter value c : " c
 
+cnt=0;
+
 a=$((RANDOM%100));
 b=$((RANDOM%100));
 c=$((RANDOM%100));
 
-ans=$(($a*$b+$c));
+ans[((cnt++))]=$(($a+$b*$c));
+ans[((cnt++))]=$(($a*$b+$c));
+ans[((cnt++))]=$(($c+$a/$b));
+ans[((cnt++))]=$(($a%$b+$c));
 
-echo $ans;
+echo ${ans[@]};
